2be801e7c0530948b5b22bbfc4ac7ddbd5341c81
Rishikesh <146565427+rishikeshghemud@users.noreply.github.com>
2025-11-07 00:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const firstName = document.querySelector('#firstName');
+	const lastName = document.querySelector('#lastName');
+	const phoneNumber = document.querySelector('#phoneNumber');
+	const email = document.querySelector('#email');
+
+	alert(
+			`${firstName.name}: ${firstName.value} ${lastName.name}: ${lastName.value} ${phoneNumber.name}: ${phoneNumber.value} ${email.name}: ${email.value}`
+		 )
+})
